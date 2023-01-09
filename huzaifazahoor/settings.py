f31ded0587c6a4b54122cd1471cdbd24406d903b
Huzaifa Zahoor <huzaifa.zahoor@mlsense.ai>
2023-01-09 13:34:47 +0500

    Added portfolio

--- huzaifazahoor/settings.py
+++ huzaifazahoor/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "portfolio"
+    "portfolio",
+    "testimonial",
 ]
 
 MIDDLEWARE = [
@@ -117,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR + STATIC_URL
+STATIC_ROOT = BASE_DIR / STATIC_URL
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- portfolio/models.py
+++ portfolio/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Portfolio(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="portfolio")
     title = models.CharField(max_length=255)
     description = models.TextField()
     client = models.CharField(max_length=20)
@@ -16,11 +16,13 @@ class Portfolio(models.Model):
 
 
 class Image(models.Model):
-    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='images/')
+    portfolio = models.ForeignKey(
+        Portfolio, on_delete=models.CASCADE, related_name="images"
+    )
+    image = models.ImageField(upload_to="images/")
     caption = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
         return self.caption
