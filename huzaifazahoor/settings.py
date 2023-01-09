80c7389cfc5977c4d2b612231f29fd7e30c051df
Huzaifa Zahoor <huzaifazahoor654@gmail.com>
2023-01-09 14:22:16 +0500

    Added design

--- huzaifazahoor/settings.py
+++ huzaifazahoor/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+# SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -118,7 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / STATIC_URL
+STATIC_ROOT = BASE_DIR / "static/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from . import models
+
+# from . import models
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
-    return render(request, "portfolio/index.html")
+    user = User.objects.prefetch_related("portfolio", "portfolio__images", "testimonials").get(
+        username="huzaifa"
+    )
+    return render(request, "portfolio/index.html", {"user": user})
