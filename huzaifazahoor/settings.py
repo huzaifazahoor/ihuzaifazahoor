57f1df5c65ce98bc404c540deb517aea7d889ce0
Huzaifa Zahoor <huzaifa.zahoor@mlsense.ai>
2023-01-13 14:57:27 +0500

    Perform Changes

--- huzaifazahoor/settings.py
+++ huzaifazahoor/settings.py
@@ -17,12 +17,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+from dotenv import dotenv_values
 
+config = dotenv_values(".env")
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-(xtjn!3)mh7p)oy)=)-=j84n4+!my%2-qanqafs=(t4_7cs3@v"
+SECRET_KEY = config["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -116,8 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = ""
+STATIC_URL = "/static/"
+# STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_DIRS = ('static',)
 
 
 # Default primary key field type
