1d3724b7b6722f2e6f37e2b88b7b5127c3a94794
Huzaifa Zahoor <huzaifa.zahoor@mlsense.ai>
2023-01-14 19:44:32 +0500

    Changed index.html

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
-
-# from . import models
 from django.contrib.auth.models import User
 
-# Create your views here.
+
 def index(request):
-    user = User.objects.prefetch_related("portfolio", "portfolio__images", "testimonials").get(
-        username="huzaifa"
-    )
+    user = User.objects.prefetch_related(
+        "portfolio", "portfolio__images", "testimonials"
+    ).get(username="huzaifa")
     return render(request, "portfolio/index.html", {"user": user})
